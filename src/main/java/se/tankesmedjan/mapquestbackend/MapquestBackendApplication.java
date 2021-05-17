95b6ade3f6561e5f3ec180707ab677975190d754
ddewe <ddewee92@gmail.com>
2021-05-17 15:15:15 +0200

    Empty project

---
+++ src/main/java/se/tankesmedjan/mapquestbackend/MapquestBackendApplication.java
@@ -0,0 +1,13 @@
+package se.tankesmedjan.mapquestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapquestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapquestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/tankesmedjan/mapquestbackend/MapquestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.tankesmedjan.mapquestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapquestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
