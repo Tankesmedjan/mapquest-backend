f922a60ef8ce51e6573f9d3755ff45839a76e4cb
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-18 13:51:10 +0200

    FindAll query added to StoryRepo

--- src/main/java/se/tankesmedjan/mapquestbackend/controllers/AdminUserController.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/controllers/AdminUserController.java
@@ -1,6 +1,5 @@
 package se.tankesmedjan.mapquestbackend.controllers;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.tankesmedjan.mapquestbackend.models.AdminUser;
--- src/main/java/se/tankesmedjan/mapquestbackend/repositories/StoryRepo.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/repositories/StoryRepo.java
@@ -3,5 +3,9 @@ package se.tankesmedjan.mapquestbackend.repositories;
 import org.springframework.data.repository.CrudRepository;
 import se.tankesmedjan.mapquestbackend.models.Story;
 
+import java.util.List;
+
 public interface StoryRepo extends CrudRepository<Story, Long> {
+
+    List<Story> findAll();
 }
