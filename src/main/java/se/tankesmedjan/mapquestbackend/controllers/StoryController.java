4db4fe8aee7bcbe2255eacecac31c7c27097a193
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-18 13:54:30 +0200

    Get endpoint added in StoryController

--- src/main/java/se/tankesmedjan/mapquestbackend/controllers/StoryController.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/controllers/StoryController.java
@@ -1,9 +1,27 @@
 package se.tankesmedjan.mapquestbackend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.tankesmedjan.mapquestbackend.models.Story;
+import se.tankesmedjan.mapquestbackend.services.StoryService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(path = "/api/story")
 public class StoryController {
+
+    private final StoryService storyService;
+
+    @Autowired
+    public StoryController(StoryService storyService){
+        this.storyService = storyService;
+    }
+
+    @GetMapping
+    public List<Story> getStories() {
+        return storyService.getStories();
+    }
 }
