27fa4f339049854b9dcbc61f1b6e8a1577799c93
ddewe <ddewee92@gmail.com>
2021-05-18 09:15:21 +0200

    Added dependency

---
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/Game.java
@@ -0,0 +1,4 @@
+package se.tankesmedjan.mapquestbackend.models;
+
+public class Game {
+}
---
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/Team.java
@@ -0,0 +1,10 @@
+package se.tankesmedjan.mapquestbackend.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+public class Team {
+
+}
