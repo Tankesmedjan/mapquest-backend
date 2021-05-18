b3b673114f96562f0d15dcfb1f82fcdd3214a6f7
ChristofferGustafsson20 <Christoffererikgustafsson@gmail.com>
2021-05-18 10:06:28 +0200

    AdminUser entity addded

---
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/AdminUser.java
@@ -0,0 +1,24 @@
+package se.tankesmedjan.mapquestbackend.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class AdminUser {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    private String username;
+    private String password;
+}
