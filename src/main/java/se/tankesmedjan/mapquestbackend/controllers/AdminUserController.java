016a6af5fc36b9b038f0c2be7bf2639cfd4a185c
ChristofferGustafsson20 <Christoffererikgustafsson@gmail.com>
2021-05-18 12:19:46 +0200

    Controller package

---
+++ src/main/java/se/tankesmedjan/mapquestbackend/controllers/AdminUserController.java
@@ -0,0 +1,8 @@
+package se.tankesmedjan.mapquestbackend.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class AdminUserController {
+}
---
+++ src/main/java/se/tankesmedjan/mapquestbackend/controllers/GameController.java
@@ -0,0 +1,4 @@
+package se.tankesmedjan.mapquestbackend.controllers;
+
+public class GameController {
+}
