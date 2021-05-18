09c000da0e81de5a49a10230eb5e1f72834630f0
ddewe <ddewee92@gmail.com>
2021-05-18 09:39:37 +0200

    Added Entity Game and Team

--- src/main/java/se/tankesmedjan/mapquestbackend/models/Game.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/Game.java
@@ -1,4 +1,30 @@
 package se.tankesmedjan.mapquestbackend.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long storyId;
+    private Long startLat;
+    private Long startLong;
+    private Long endLat;
+    private Long endLong;
+    private String userId;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "game")
+    @JsonIgnore
+    private List<Team> team;
 }
--- src/main/java/se/tankesmedjan/mapquestbackend/models/Team.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/Team.java
@@ -1,10 +1,24 @@
 package se.tankesmedjan.mapquestbackend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Entity
+@Getter
+@Setter
 public class Team {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String teamName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Game game;
 }
