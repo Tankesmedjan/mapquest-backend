51da886ab6225b201f6ae9c691f0933fdf743af9
ddewe <ddewee92@gmail.com>
2021-05-18 13:38:50 +0200

    Fixed Rest endpoint

--- src/main/java/se/tankesmedjan/mapquestbackend/controllers/AdminUserController.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/controllers/AdminUserController.java
@@ -1,17 +1,20 @@
 package se.tankesmedjan.mapquestbackend.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.tankesmedjan.mapquestbackend.models.AdminUser;
 import se.tankesmedjan.mapquestbackend.services.AdminUserService;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api/admin")
 public class AdminUserController {
 
     private final AdminUserService adminUserService;
 
+    @Autowired
     public AdminUserController(AdminUserService adminUserService) {
         this.adminUserService = adminUserService;
     }
