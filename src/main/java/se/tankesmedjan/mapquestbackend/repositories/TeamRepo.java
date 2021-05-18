0d91e377b8aca8a4b2fa1f80da96427308b92e1a
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-18 14:55:46 +0200

    Endpoints added to TeamController

--- src/main/java/se/tankesmedjan/mapquestbackend/repositories/TeamRepo.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/repositories/TeamRepo.java
@@ -3,5 +3,10 @@ package se.tankesmedjan.mapquestbackend.repositories;
 import org.springframework.data.repository.CrudRepository;
 import se.tankesmedjan.mapquestbackend.models.Team;
 
+import java.util.List;
+
 public interface TeamRepo extends CrudRepository<Team, Long> {
+
+    List<Team> findAll();
+    Team findTeamById(Long id);
 }
--- src/main/java/se/tankesmedjan/mapquestbackend/services/TeamService.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/services/TeamService.java
@@ -2,7 +2,10 @@ package se.tankesmedjan.mapquestbackend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.tankesmedjan.mapquestbackend.models.Team;
 import se.tankesmedjan.mapquestbackend.repositories.TeamRepo;
+
+import java.util.List;
 
 @Service
 public class TeamService {
@@ -12,5 +15,19 @@ public class TeamService {
     @Autowired
     public TeamService(TeamRepo teamRepo){
         this.teamRepo = teamRepo;
+    }
+
+    public Team addTeams(Team team) {
+        return teamRepo.save(team);
+    }
+
+    public List<Team> getTeams() {
+        return teamRepo.findAll();
+    }
+
+    public Team deleteTeam(Long id) {
+        Team team = teamRepo.findTeamById(id);
+        teamRepo.delete(team);
+        return team;
     }
 }
