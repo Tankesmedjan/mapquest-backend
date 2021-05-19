f4fbdc60747ac510b9302a9319a2f273e5c24bec
ddewe <ddewee92@gmail.com>
2021-05-19 10:39:51 +0200

    Added documentation for TeamService

--- src/main/java/se/tankesmedjan/mapquestbackend/services/TeamService.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/services/TeamService.java
@@ -17,14 +17,28 @@ public class TeamService {
         this.teamRepo = teamRepo;
     }
 
+    /**
+     * Basic method for adding a Team.
+     * @param team the body of a Team.
+     * @return saves the Team.
+     */
     public Team addTeams(Team team) {
         return teamRepo.save(team);
     }
 
+    /**
+     * Basic method for getting a list of all Teams.
+     * @return the list of all Teams.
+     */
     public List<Team> getTeams() {
         return teamRepo.findAll();
     }
 
+    /**
+     * Basic method for deleting a specific team.
+     * @param id the ID of the Team to be deleted.
+     * @return deletes the chosen Team.
+     */
     public Team deleteTeam(Long id) {
         Team team = teamRepo.findTeamById(id);
         teamRepo.delete(team);
