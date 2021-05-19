24a0127020e81f5067c27c2be73940c17ce71a59
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-19 12:10:09 +0200

    Removed entity and relations from TeamPlayer

--- src/main/java/se/tankesmedjan/mapquestbackend/models/Player.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/models/Player.java
@@ -3,7 +3,6 @@ package se.tankesmedjan.mapquestbackend.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
--- src/main/java/se/tankesmedjan/mapquestbackend/repositories/GameMissionRepo.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/repositories/GameMissionRepo.java
@@ -3,12 +3,10 @@ package se.tankesmedjan.mapquestbackend.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import se.tankesmedjan.mapquestbackend.models.GameMission;
-import se.tankesmedjan.mapquestbackend.models.Mission;
 
 import java.util.List;
 
 public interface GameMissionRepo extends CrudRepository<GameMission, Long> {
-
 
     @Query("SELECT gm FROM GameMission gm " +
             "inner join Mission m on m.id = gm.missionId.id " +
