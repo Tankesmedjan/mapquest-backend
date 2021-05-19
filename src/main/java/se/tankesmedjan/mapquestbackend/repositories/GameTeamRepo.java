6a496b64d85ee6464b71b1c6f9c95128574230c8
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-19 21:34:13 +0200

    Removed GameTeam Entity, controller, repo and service.

--- src/main/java/se/tankesmedjan/mapquestbackend/repositories/GameTeamRepo.java
+++
@@ -1,7 +0,0 @@
-package se.tankesmedjan.mapquestbackend.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import se.tankesmedjan.mapquestbackend.models.GameTeam;
-
-public interface GameTeamRepo extends CrudRepository<GameTeam, Long> {
-}
