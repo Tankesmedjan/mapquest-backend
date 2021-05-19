9158908811410bd97ae1370c77eef21abba7b586
ddewe <ddewee92@gmail.com>
2021-05-19 10:33:37 +0200

    Added documentation for MissionService

--- src/main/java/se/tankesmedjan/mapquestbackend/services/MissionService.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/services/MissionService.java
@@ -17,14 +17,28 @@ public class MissionService {
         this.missionRepo = missionRepo;
     }
 
+    /**
+     * Basic method for adding a Mission.
+     * @param mission the body of a Mission.
+     * @return saves the Mission.
+     */
     public Mission addMissions(Mission mission) {
        return missionRepo.save(mission);
     }
 
+    /**
+     * Basic method for getting a list of all Missions.
+     * @return the list of all Missions.
+     */
     public List<Mission> getAllMissions() {
         return missionRepo.findAll();
     }
 
+    /**
+     * Basic method for deleting a specific mission.
+     * @param id the ID of the Mission to be deleted.
+     * @return the Mission to be deleted.
+     */
     public Mission deleteMission(Long id) {
         Mission mission = missionRepo.findMissionById(id);
         missionRepo.delete(mission);
