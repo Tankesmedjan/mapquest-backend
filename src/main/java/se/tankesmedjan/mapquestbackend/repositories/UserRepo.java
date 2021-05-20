c4234d266d1a03ccd68004d80b39f81fbece45cf
ddewe <ddewee92@gmail.com>
2021-05-20 11:49:31 +0200

    Added delete endpoint for UserController

--- src/main/java/se/tankesmedjan/mapquestbackend/repositories/UserRepo.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/repositories/UserRepo.java
@@ -1,6 +1,7 @@
 package se.tankesmedjan.mapquestbackend.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import se.tankesmedjan.mapquestbackend.models.AdminUser;
 import se.tankesmedjan.mapquestbackend.models.User;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface UserRepo extends CrudRepository<User, String> {
 
     List<User> findAll();
+    User findUserById(String id);
 }
