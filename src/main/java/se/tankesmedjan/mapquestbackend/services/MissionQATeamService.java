6155e02bb9f38f9f99fe48d81c98157a7bb51907
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-21 13:39:39 +0200

    Documentation added to MissionQATeamService

--- src/main/java/se/tankesmedjan/mapquestbackend/services/MissionQATeamService.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/services/MissionQATeamService.java
@@ -20,15 +20,29 @@ public class MissionQATeamService {
         this.missionQATeamRepo = missionQATeamRepo;
     }
 
+    /**
+     * Basic method to add a missionQATeam.
+     * @param missionQATeam the body of a missionQATeam.
+     * @return saves the missionQATeam.
+     */
     public MissionQATeamDTO addMissionQATeam(MissionQATeamDTO missionQATeam) {
         missionQATeamRepo.save(MissionQATeamMapper.INSTANCE.dtoToMissionQATeam(missionQATeam));
         return missionQATeam;
     }
 
+    /**
+     * Basic method to get a list of all missionQATeams.
+     * @return the list of all missionQATeams.
+     */
     public List<MissionQATeam> getMissionQATeam() {
         return missionQATeamRepo.findAll();
     }
 
+    /**
+     * Basic method for deleting a specific missionQATeam.
+     * @param id the id of the missionQATeam to be deleted.
+     * @return deletes the chosen missionQATeam.
+     */
     public MissionQATeam deleteMissionQATeam(String id) {
         MissionQATeam missionQATeam = missionQATeamRepo.findMissionQATeamById(id);
         missionQATeamRepo.delete(missionQATeam);
