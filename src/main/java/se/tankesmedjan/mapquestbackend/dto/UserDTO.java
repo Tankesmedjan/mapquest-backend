70102f5b9bc82d99e6cd0ad37038817802518fde
Richardsonn1 <marcus.richardson@hotmail.se>
2021-05-26 14:56:31 +0200

    removed hash256

--- src/main/java/se/tankesmedjan/mapquestbackend/dto/UserDTO.java
+++ src/main/java/se/tankesmedjan/mapquestbackend/dto/UserDTO.java
@@ -16,13 +16,6 @@ public class UserDTO {
     private String password;
     private Timestamp timestampActivation;
     private Timestamp timestampExpired;
-
-    public void setPassword(String password) {
-        String sha256hex = Hashing.sha256()
-                .hashString(password, StandardCharsets.UTF_8)
-                .toString();
-        this.password = sha256hex;
-    }
 }
 
 
